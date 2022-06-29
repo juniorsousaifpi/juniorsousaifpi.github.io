c990e1cedcf53319590163ff55820c34c3b2c504
Your Name <you@example.com>
2022-06-29 20:23:52 -0300

    mesagem

---
+++ Q04.py.py
@@ -0,0 +1,20 @@
+# from random import choice
+
+
+
+# Jogador1=str(input("Digite o nome do jogador 1: "))
+# jogador2=str(input("Digite o nome do jogador 2: "))
+# cartas=["A", "J", "Q", "K","2", "3", "4", "5", "6", "7" , "8", "9","10"]
+# naipes=["E","P","O","C"]
+# deck=[]
+# cartas1 = []
+
+# for i in naipes:
+#     for c in cartas:
+#         deck.append(f'{c}{i}')
+    
+# print(deck)
+# for i in range(3):
+#     cartas1.append(choice(deck))
+# print(cartas1)
+
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # juniorsousaifpi.github.io
 
 ### Comandos para enviar alterações para o Github
-
+<!-- 
 1. git add .
 2. git commit -m "mensagem"
-3. git push
+3. git push -->
